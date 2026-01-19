a58a825f474eb79476ff247a0847007db8cf6e89
虜漣踪 <lorenzo.bellier@gmail.com>
2026-01-19 14:57:12 +0100

    Add user creation automation script

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# Script to create multiple users and set passwords
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+for user in user1 user2 user3
+do
+  useradd -m $user
+  echo "$user:password123" | chpasswd
+  echo "User $user created."
+done
