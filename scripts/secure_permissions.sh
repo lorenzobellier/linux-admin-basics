1981489d2bf0c79693c2d6739a9811b6fcc926a7
虜漣踪 <lorenzo.bellier@gmail.com>
2026-01-19 14:59:48 +0100

    Add secure directory permissions script

--- scripts/secure_permissions.sh
+++ scripts/secure_permissions.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+# Script to secrure a directory
+
+DIRECTORY="/var/secure_data"
+
+mkdir -p $DIRECTORY
+chmod 700 $DIRECTORY
+chown root:root $DIRECTORY
+
+echo "Secure directory created at $Directory"
