b9aa171b5a2c5976780a10b4b5d9005ec2d10b8b
虜漣踪 <lorenzo.bellier@gmail.com>
2026-01-19 15:03:28 +0100

    Add home directory backup script

--- scripts/backup_home.sh
+++ scripts/backup_home.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+# Script to backup /home directory
+
+BACKUP_DIR="/var/backups/home_backup"
+DATE=$(date +%F)
+
+mkdir -p $BACKUP_DIR
+tar -czvf $BACKUP_DIR/home_backup_$DATE.tar.gz /home
+
+echo "Backup completed: $BACKUP_DIR/home_backup_$DATE.tar.gz"
